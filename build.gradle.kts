0015793609505d92dd97cea2f6970a27effe13d0
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2021-11-10 19:14:16 +0100

    Update Kotlin, Gradle, React Add all required imports for App.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.5.0"
+    kotlin("js") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven("https://dl.bintray.com/kotlin/kotlin-eap")
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
-    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
     mavenCentral()
-    jcenter()
 }
 
 kotlin {
@@ -27,26 +24,31 @@ kotlin {
 dependencies {
 
     //React, React DOM + Wrappers (chapter 3)
-    implementation("org.jetbrains:kotlin-react:17.0.2-pre.154-kotlin-1.5.0")
-    implementation("org.jetbrains:kotlin-react-dom:17.0.2-pre.154-kotlin-1.5.0")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.264-kotlin-1.5.31")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.264-kotlin-1.5.31")
     implementation(npm("react", "17.0.2"))
     implementation(npm("react-dom", "17.0.2"))
 
     //Kotlin Styled (chapter 3)
-    implementation("org.jetbrains:kotlin-styled:5.2.3-pre.154-kotlin-1.5.0")
-    implementation(npm("styled-components", "~5.2.3"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.3-pre.264-kotlin-1.5.31")
+    implementation(npm("styled-components", "~5.3.3"))
 
     //Video Player (chapter 7)
-    implementation(npm("react-youtube-lite", "1.0.1"))
+    implementation(npm("react-youtube-lite", "1.1.0"))
 
     //Share Buttons (chapter 7)
-    implementation(npm("react-share", "~4.2.1"))
+    implementation(npm("react-share", "4.4.0"))
 
-    //Coroutines (chapter 8)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-RC")
+    //Coroutines & serialization (chapter 8)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 }
 
 // Heroku Deployment (chapter 9)
 tasks.register("stage") {
     dependsOn("build")
+}
+
+rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+    versions.webpackCli.version = "4.9.0"
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
+import react.dom.*
 import kotlinx.browser.document
+import kotlinx.css.*
+import kotlinx.serialization.Serializable
+import styled.*
 
 fun main() {
     document.bgColor = "blue"
