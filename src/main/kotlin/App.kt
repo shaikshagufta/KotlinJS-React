10f4683382b6903d39fd9497620a2b01795e47fc
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-19 22:09:53 +0530

    Adding props

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -20,7 +20,12 @@ val app = fc<Props> {
         h3 {
             +"Videos to watch"
         }
-        child(videoList)
+// let's make sure we instantiate the child components with proper attributes.
+        child(videoList){
+            attrs {
+                videos = unwatchedVideos
+            }
+        }
         //we can now write a Kotlin for-loop to iterate over the collection of unwatched and watched videos.
         for (video in unwatchedVideos) {
             p {
@@ -31,7 +36,11 @@ val app = fc<Props> {
         h3 {
             +"Videos watched"
         }
-        child(videoList)
+        child(videoList){
+            attrs {
+                videos = unwatchedVideos
+            }
+        }
         for (video in watchedVideos) {
             p {
                 +"${video.speaker}: ${video.title}"
@@ -56,3 +65,8 @@ val app = fc<Props> {
 }
 //problem: The app component has no control over the content that is shown by the videoList component.
 // It is hard-coded, so we see the same list twice.
+
+//Solution: So Instead, we need a mechanism to pass the list into the component.
+
+//We now have a reusable component that can render a list of videos.
+// The code for our app component got smaller, and we got rid of some duplication in our code.
--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -9,10 +9,20 @@ import react.dom.*
 //The videoList component follows the same pattern as the app component.
 // It uses the fc builder function, and contains the code from our unwatchedVideos list.
 
-val videoList = fc<Props> {
-    for (video in unwatchedVideos) {
+//We now adjust the class definition of VideoList to make use of the props, which are passed into the fc block
+val videoList = fc<VideoListProps> { props ->
+    for (video in props.videos) {
         p {
+            key = video.id.toString()
+//'Key' attribute helps the React renderer figure out what it needs to do when the value of props.videos changes.
+// It uses the key to determine which parts of a list need to refresh, and which ones stay the same.
             +"${video.speaker}: ${video.title}"
         }
     }
+}
+//For our videoList, we want to add a prop containing the list of videos to be shown.
+// To do so, let's define an external interface that holds all the props which can be passed to a videoList component.
+
+external interface VideoListProps : Props {
+    var videos: List<Video>
 }
