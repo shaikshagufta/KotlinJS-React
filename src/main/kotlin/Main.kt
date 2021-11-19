7418963104d41290c5ac4c2622b5ee19f80e443b
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-19 19:52:37 +0530

    A first static page with React stage1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,8 +6,39 @@ import styled.*
 
 fun main() {
     render(document.getElementById("root")) {
-        h1{
-            +"Hello, React+Kotlin/JS from T4!"
+        h1 {
+            +"KotlinConf Explorer"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            p {
+                +"John Doe: Building and breaking things"
+            }
+            p {
+                +"Jane Smith: The development process"
+            }
+            p {
+                +"Matt Miller: The Web 7.0"
+            }
+
+            h3 {
+                +"Videos watched"
+            }
+            p {
+                +"Tom Jerry: Mouseless development"
+            }
+        }
+        div {
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
         }
     }
 }
