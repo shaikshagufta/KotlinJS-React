6be9286375f67901c700bcec1eddfb31e500fa94
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-19 20:11:51 +0530

    Using Kotlin language constructs in markup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,21 +13,20 @@ fun main() {
             h3 {
                 +"Videos to watch"
             }
-            p {
-                +"John Doe: Building and breaking things"
-            }
-            p {
-                +"Jane Smith: The development process"
-            }
-            p {
-                +"Matt Miller: The Web 7.0"
+            //we can now write a Kotlin for-loop to iterate over the collection of unwatched and watched videos.
+            for(video in unwatchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
 
             h3 {
                 +"Videos watched"
             }
-            p {
-                +"Tom Jerry: Mouseless development"
+            for(video in watchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
         }
         div {
@@ -42,3 +41,19 @@ fun main() {
         }
     }
 }
+data class Video(
+    val id: Int,
+    val title: String,
+    val speaker: String,
+    val videoUrl: String
+)
+//let's fill up the two lists for unwatched videos and watched videos respectively.
+val unwatchedVideos = listOf(
+    Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+    Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+    Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+)
+
+val watchedVideos = listOf(
+    Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+)
