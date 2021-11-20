9b7f895d9b3fa1f81ad871c9f0f1894b49a27d17
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-20 23:25:30 +0530

    More components--Adding a button and wiring it

--- src/main/kotlin/VideoPlayer.kt
+++ src/main/kotlin/VideoPlayer.kt
@@ -4,14 +4,25 @@ A video player needs to know the talk title, the author of the talk, and the lin
 All this information is already contained in a Video object.
 We can just pass one of those as a prop, and access its attributes.
 */
+
 import kotlinx.css.*
 import kotlinx.html.js.onClickFunction
 import react.*
 import react.dom.*
 import styled.*
+/*
+Let's add a button to mark a video as (un)watched,
+to move it between the two lists.
+*/
 
+/*
+we want the button to look different based on whether a video has already been watched or not.
+This is also information we need to pass as a prop.
+*/
 external interface VideoPlayerProps : Props {
     var video: Video
+    var onWatchedButtonPressed: (Video) -> Unit
+    var unwatchedVideo: Boolean
 }
 
 val videoPlayer = fc<VideoPlayerProps> { props ->
@@ -23,6 +34,24 @@ val videoPlayer = fc<VideoPlayerProps> { props ->
         }
         h3 {
             +"${props.video.speaker}: ${props.video.title}"
+        }
+        //adding the button to the actual component.
+        styledButton {
+            css {
+                display = Display.block
+                backgroundColor = if(props.unwatchedVideo) Color.lightGreen else Color.red
+            }
+            attrs {
+                onClickFunction = {
+                    props.onWatchedButtonPressed(props.video)
+                }
+            }
+            if(props.unwatchedVideo) {
+                +"Mark as watched"
+            }
+            else {
+                +"Mark as unwatched"
+            }
         }
         img {
             attrs {
